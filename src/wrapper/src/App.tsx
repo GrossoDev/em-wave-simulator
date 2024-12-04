465d948c516a4ad8ec40a299fd0ba52c825a63b6
Giuliano Rosso <rossogiuliano@gmail.com>
2024-12-04 00:14:36 -0300

    Added Wrapper

---
+++ src/wrapper/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import UnityGame from "./UnityGame.tsx";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <UnityGame />
+      </div>
+    </>
+  );
+}
+
+export default App;
